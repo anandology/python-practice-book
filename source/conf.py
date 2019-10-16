4cc258047b20053972e0cee9e1dfd1f7e2dde40f
Anand Chitipothu <anandology@gmail.com>
2019-10-16 08:33:08 +0530

    Added sphinx-rtd-theme

--- source/conf.py
+++ source/conf.py
@@ -14,6 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -22,7 +23,7 @@ copyright = '2019, Anand Chitipothu'
 author = 'Anand Chitipothu'
 
 # The full version, including alpha/beta/rc tags
-release = '19.09'
+release = '0.3'
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,7 +32,8 @@ release = '19.09'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'problem'
+    'problem',
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -48,7 +50,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
